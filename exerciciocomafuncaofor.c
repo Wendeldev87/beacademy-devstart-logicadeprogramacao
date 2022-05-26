70cd10b6dc238265a701861587deabc7b6aac3b9
Wendel Duarte <105465397+Wendeldev87@users.noreply.github.com>
2022-05-26 09:18:57 -0300

    Add files via upload

---
+++ exerciciocomafuncaofor.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main (){
+    int i;
+    for(i = 1); i <= 10; i++) {
+    printf("%d/n",i); 
+
+    }
+    
+    system("pause");
+    return 0;
+}
